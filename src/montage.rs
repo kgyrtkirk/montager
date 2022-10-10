2fedc1f96c9c79fccb670551346c41717463aed9
Zoltan Haindrich <zhaindrich@cloudera.com>
2022-10-10 13:32:17 +0200

    some

--- src/montage.rs
+++ src/montage.rs
@@ -21,39 +21,58 @@ struct MontageImage {
 
 #[allow(unused)]
 impl MontageImage {
-    fn new(file_name: &String) -> MontageImage {
+
+    fn new(file_name: &String,pos:&Point2i) -> MontageImage {
         let image = imgcodecs::imread(&file_name, 1).unwrap();
+        // image.convert_to(m, rtype, alpha, beta)
+        // imgcodecs::conv
         if image.size().unwrap().width<=0 {
             panic!("Can't open file"); // FIXME: show filename
         }
 
         MontageImage {
             file_name: file_name.clone(),
-            position: Point2i::new(1, 2),
+            position: pos.clone(),
             image: image.clone(),
-            image2: image.clone(),
+            image2: Mat::default(),
         }
     }
     fn render(&mut self, size : Size) -> Result<()>{
-        // self.image2 = Mat::zeros_size(size, CV_8UC3).unwrap().to_mat().unwrap();
+        self.image2 = Mat::zeros_size(size, CV_8UC3).unwrap().to_mat().unwrap();
         // //let size = self.image.size().unwrap();
         // // self.image2.set_rows(size.width);
-        // let m = imgproc::get_rotation_matrix_2d(Point2f::new(50.0, 50.0), 10.0, 1.0).unwrap();
+        let m = imgproc::get_rotation_matrix_2d(Point2f::new(50.0, 50.0), 10.0, 1.0).unwrap();
 
         // let size= Size ::new(500,500);
         // println!("s: {:?}", size);
-        // imgproc::warp_affine(
-        //     &self.image,
-        //     &mut self.image2,
-        //     &m,
-        //     size,
-        //     INTER_LINEAR,
-        //     BORDER_CONSTANT,
-        //     Scalar::new(0.0, 0.0, 0.0, 0.0),
-        // );
+        imgproc::warp_affine(
+            &self.image,
+            &mut self.image2,
+            &m,
+            size,
+            INTER_LINEAR,
+            BORDER_CONSTANT,
+            Scalar::new(0.0, 0.0, 0.0, 0.0),
+        );
         // println!("s: {:?}", self.image2.size());
-        self.image.copy_to(&mut self.image2);
+        // self.image.copy_to(&mut self.image2);
+        let pt1 = Point2i::new(0, 0);
+        let pt2 = self.position;
+        let pt3 = Point2i::new(300, 150);
+        let color = Scalar::new(255., 255., 255., 255.);
+        imgproc::line(&mut self.image2, pt1, pt2, color, 8, LINE_8, 1);
+        imgproc::line(&mut self.image2, pt2, pt3, color, 8, LINE_8, 1);
+
         Ok({})
+    }
+
+    fn dist(self : &MontageImage,p:&Point2i) -> f64{
+        let d=(self.position-*p).norm();
+        d
+    }
+    fn sample(self : &MontageImage,p:&Point2i) -> Result<&Vec3b>{
+        let q=self.image2.at_2d::<Vec3b>(p.x,p.y)?;
+        Ok(q)
     }
 }
 
@@ -68,17 +87,20 @@ struct Montage {
 impl Montage {
     fn new(file_name: &String) -> Montage {
         let size = Size_ {
-            width: 600  ,
-            height: 600,
+            width: 500  ,
+            height: 500,
         };
-        let m = MontageImage::new(&String::from("r.png"));
+        // let m = MontageImage::new(&String::from("r.png"));
         let mut image = Mat::zeros_size(size, CV_8UC3).unwrap().to_mat().unwrap();
         let color = Scalar::new(255., 0., 255., 0.);
         let pt1 = Point2i::new(100, 100);
         let pt2 = Point2i::new(200, 50);
         let pt3 = Point2i::new(300, 150);
         imgproc::line(&mut image, pt1, pt2, color, 8, LINE_8, 1);
-        let images = vec![(m)];
+        let images = vec![
+            MontageImage::new(&String::from("r2.png"),&Point2i::new(300, 100)),
+            MontageImage::new(&String::from("r3.png"),&Point2i::new(100, 200)),
+            ];
         // let images = vec![Box::new(m)];
         let mut m = Montage {
             image: image,
@@ -94,13 +116,49 @@ impl Montage {
         
         self.images.iter_mut().for_each( |m| m.render(self.size).unwrap() );
 
+        self.image = Mat::new_size_with_default(self.size, CV_8UC3,  Scalar::from(127))?;
+
+        println!("ok?");
+        let v0 = VecN::new(1,2,3,4);
+        let v = Vec3b::from([255,0,255]);
+        for row in 0..self.size.height {
+        for col in 0..self.size.width {
+                let p : Point2i =Point_ { x: row, y: col };
+                let mut dist_color : Vec<(f64,&Vec3b)> = self.images.iter().map( |i| (i.dist(&p),i.sample(&p).unwrap()) ).collect();
+
+                    let a = 0.;
+                    // cmp::
+                    // 1.cmp(2);
+                    // 1.0.cmp
+                    // float_cmp
+                dist_color.sort_by( |(d,v),(d2,v2)| d.total_cmp(d2));
+
+
+                // println!("{:?}",dist_color);
+
+
+                // for img in self.images.iter_mut() {
+
+                // }
+
+                // let q=self.image.at_2d::<Vec3b>(row-1000, col)?;
+                let q=dist_color.get(0).unwrap().1;
+                //self.image[1,2];
+                *self.image.at_2d_mut(row, col)?=*q;
+        
+            }
+        }
+        println!("ok");
+        println!("ok");
+
+        if(false) {
         for i in self.images.iter_mut() {
             // i.render();
-            let image = &i.image2;
+            let image = &i.image;
             let size = image.size().unwrap();
             let roi = Rect {
-                x: 100,
-                y: 100,
+                x: i.position.x,
+                y: i.position.y,
                 width: size.width,
                 height: size.height,
             };
@@ -109,11 +167,13 @@ impl Montage {
             println!("{:?}",dest);
             println!("{:?}",image);
   //           i.image2.copy_to(&mut dest)?;
-            image.copy_to(&mut dest)?;
+             image.copy_to(&mut dest)?;
             // normalize(src, dst, alpha, beta, norm_type, dtype, mask)
             // panic!("asd");
             // self.image.re
         }
+        }
+        
         Ok({})
     }
 }
@@ -170,8 +230,10 @@ pub fn editor(file_name: &Vec<String>) -> Result<()> {
             break;
         }
         let mut editor = montage.lock().unwrap();
-        editor.montage.render();
+        editor.montage.render()?;
         imshow(window, &editor.montage.image)?;
+//        imshow(window, &editor.montage.images.get(0).unwrap().image2)?;
+//        imshow(window, &editor.montage.image)?;
         // if key == 's' as i32 {
         //     annotation_editor.save();
         //     break;
