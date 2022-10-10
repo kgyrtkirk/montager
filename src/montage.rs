c4ce8a3cc34b854e8b22952a323836e8becf07c9
Zoltan Haindrich <zhaindrich@cloudera.com>
2022-10-10 13:32:47 +0200

    format

--- src/montage.rs
+++ src/montage.rs
@@ -5,7 +5,7 @@ use opencv::{
     core::*,
     highgui::{self, imshow, resize_window, EVENT_LBUTTONDOWN, WINDOW_GUI_EXPANDED},
     imgcodecs,
-    imgproc::{self,  INTER_LINEAR, LINE_8},
+    imgproc::{self, INTER_LINEAR, LINE_8},
     prelude::*,
     types::VectorOfPoint,
     Result,
@@ -21,12 +21,11 @@ struct MontageImage {
 
 #[allow(unused)]
 impl MontageImage {
-
-    fn new(file_name: &String,pos:&Point2i) -> MontageImage {
+    fn new(file_name: &String, pos: &Point2i) -> MontageImage {
         let image = imgcodecs::imread(&file_name, 1).unwrap();
         // image.convert_to(m, rtype, alpha, beta)
         // imgcodecs::conv
-        if image.size().unwrap().width<=0 {
+        if image.size().unwrap().width <= 0 {
             panic!("Can't open file"); // FIXME: show filename
         }
 
@@ -37,7 +36,7 @@ impl MontageImage {
             image2: Mat::default(),
         }
     }
-    fn render(&mut self, size : Size) -> Result<()>{
+    fn render(&mut self, size: Size) -> Result<()> {
         self.image2 = Mat::zeros_size(size, CV_8UC3).unwrap().to_mat().unwrap();
         // //let size = self.image.size().unwrap();
         // // self.image2.set_rows(size.width);
@@ -66,12 +65,12 @@ impl MontageImage {
         Ok({})
     }
 
-    fn dist(self : &MontageImage,p:&Point2i) -> f64{
-        let d=(self.position-*p).norm();
+    fn dist(self: &MontageImage, p: &Point2i) -> f64 {
+        let d = (self.position - *p).norm();
         d
     }
-    fn sample(self : &MontageImage,p:&Point2i) -> Result<&Vec3b>{
-        let q=self.image2.at_2d::<Vec3b>(p.x,p.y)?;
+    fn sample(self: &MontageImage, p: &Point2i) -> Result<&Vec3b> {
+        let q = self.image2.at_2d::<Vec3b>(p.x, p.y)?;
         Ok(q)
     }
 }
@@ -87,7 +86,7 @@ struct Montage {
 impl Montage {
     fn new(file_name: &String) -> Montage {
         let size = Size_ {
-            width: 500  ,
+            width: 500,
             height: 500,
         };
         // let m = MontageImage::new(&String::from("r.png"));
@@ -98,82 +97,84 @@ impl Montage {
         let pt3 = Point2i::new(300, 150);
         imgproc::line(&mut image, pt1, pt2, color, 8, LINE_8, 1);
         let images = vec![
-            MontageImage::new(&String::from("r2.png"),&Point2i::new(300, 100)),
-            MontageImage::new(&String::from("r3.png"),&Point2i::new(100, 200)),
-            ];
+            MontageImage::new(&String::from("r2.png"), &Point2i::new(300, 100)),
+            MontageImage::new(&String::from("r3.png"), &Point2i::new(100, 200)),
+        ];
         // let images = vec![Box::new(m)];
         let mut m = Montage {
             image: image,
             images: images,
             size: size,
         };
-//        Montage::render(&mut m);
+        //        Montage::render(&mut m);
         m.render();
         imgproc::line(&mut m.image, pt1, pt3, color, 8, LINE_8, 1);
         m
     }
-    fn render(&mut self) -> Result<()>{
-        
-        self.images.iter_mut().for_each( |m| m.render(self.size).unwrap() );
+    fn render(&mut self) -> Result<()> {
+        self.images
+            .iter_mut()
+            .for_each(|m| m.render(self.size).unwrap());
 
-        self.image = Mat::new_size_with_default(self.size, CV_8UC3,  Scalar::from(127))?;
+        self.image = Mat::new_size_with_default(self.size, CV_8UC3, Scalar::from(127))?;
 
         println!("ok?");
-        let v0 = VecN::new(1,2,3,4);
-        let v = Vec3b::from([255,0,255]);
+        let v0 = VecN::new(1, 2, 3, 4);
+        let v = Vec3b::from([255, 0, 255]);
         for row in 0..self.size.height {
-        for col in 0..self.size.width {
-                let p : Point2i =Point_ { x: row, y: col };
-                let mut dist_color : Vec<(f64,&Vec3b)> = self.images.iter().map( |i| (i.dist(&p),i.sample(&p).unwrap()) ).collect();
+            for col in 0..self.size.width {
+                let p: Point2i = Point_ { x: row, y: col };
+                let mut dist_color: Vec<(f64, &Vec3b)> = self
+                    .images
+                    .iter()
+                    .map(|i| (i.dist(&p), i.sample(&p).unwrap()))
+                    .collect();
 
-                    let a = 0.;
-                    // cmp::
-                    // 1.cmp(2);
-                    // 1.0.cmp
-                    // float_cmp
-                dist_color.sort_by( |(d,v),(d2,v2)| d.total_cmp(d2));
-
+                let a = 0.;
+                // cmp::
+                // 1.cmp(2);
+                // 1.0.cmp
+                // float_cmp
+                dist_color.sort_by(|(d, v), (d2, v2)| d.total_cmp(d2));
 
                 // println!("{:?}",dist_color);
-
 
                 // for img in self.images.iter_mut() {
 
                 // }
 
                 // let q=self.image.at_2d::<Vec3b>(row-1000, col)?;
-                let q=dist_color.get(0).unwrap().1;
+                let q = dist_color.get(0).unwrap().1;
                 //self.image[1,2];
-                *self.image.at_2d_mut(row, col)?=*q;
-        
+                *self.image.at_2d_mut(row, col)? = *q;
             }
         }
         println!("ok");
         println!("ok");
 
-        if(false) {
-        for i in self.images.iter_mut() {
-            // i.render();
-            let image = &i.image;
-            let size = image.size().unwrap();
-            let roi = Rect {
-                x: i.position.x,
-                y: i.position.y,
-                width: size.width,
-                height: size.height,
-            };
-            
-            let mut dest = Mat::roi(&mut self.image, roi)?;
-            println!("{:?}",dest);
-            println!("{:?}",image);
-  //           i.image2.copy_to(&mut dest)?;
-             image.copy_to(&mut dest)?;
-            // normalize(src, dst, alpha, beta, norm_type, dtype, mask)
-            // panic!("asd");
-            // self.image.re
+        if (false) {
+            for i in self.images.iter_mut() {
+                // i.render();
+                let image = &i.image;
+                let size = image.size().unwrap();
+                let roi = Rect {
+                    x: i.position.x,
+                    y: i.position.y,
+                    width: size.width,
+                    height: size.height,
+                };
+
+                let mut dest = Mat::roi(&mut self.image, roi)?;
+                println!("{:?}", dest);
+                println!("{:?}", image);
+                //           i.image2.copy_to(&mut dest)?;
+                image.copy_to(&mut dest)?;
+                // normalize(src, dst, alpha, beta, norm_type, dtype, mask)
+                // panic!("asd");
+                // self.image.re
+            }
         }
-        }
-        
+
         Ok({})
     }
 }
@@ -232,8 +233,8 @@ pub fn editor(file_name: &Vec<String>) -> Result<()> {
         let mut editor = montage.lock().unwrap();
         editor.montage.render()?;
         imshow(window, &editor.montage.image)?;
-//        imshow(window, &editor.montage.images.get(0).unwrap().image2)?;
-//        imshow(window, &editor.montage.image)?;
+        //        imshow(window, &editor.montage.images.get(0).unwrap().image2)?;
+        //        imshow(window, &editor.montage.image)?;
         // if key == 's' as i32 {
         //     annotation_editor.save();
         //     break;
