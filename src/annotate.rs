ea9ebba9c3589f502ed4eca67af86ad45a24aca4
Zoltan Haindrich <zhaindrich@cloudera.com>
2022-10-12 14:19:54 +0200

    format file

--- src/annotate.rs
+++ src/annotate.rs
@@ -1,25 +1,25 @@
 use std::sync::{Arc, Mutex};
 
 use opencv::{
-	highgui::{self, imshow, WINDOW_GUI_EXPANDED, EVENT_LBUTTONDOWN, resize_window},
-	prelude::*,
-	Result,
+    core::*,
+    highgui::{self, imshow, resize_window, EVENT_LBUTTONDOWN, WINDOW_GUI_EXPANDED},
     imgcodecs,
-    imgproc::{self, LINE_8, convex_hull},
-    core::*, types::VectorOfPoint,
+    imgproc::{self, convex_hull, LINE_8},
+    prelude::*,
+    types::VectorOfPoint,
+    Result,
 };
 use serde::{Deserialize, Serialize};
 use serde_yaml::{self};
 
-
 #[allow(unused)]
 #[derive(Debug, Serialize, Deserialize)]
 struct OnDiskAnnotations {
-    points : Vec<(i32,i32)>,
+    points: Vec<(i32, i32)>,
 }
 
 pub struct AnnotationEditor {
-    points : Arc<Mutex<VectorOfPoint>>,// = Arc::new(Mutex::new(VectorOfPoint::new()));
+    points: Arc<Mutex<VectorOfPoint>>, // = Arc::new(Mutex::new(VectorOfPoint::new()));
     annotation_file: String,
     #[allow(unused)]
     file_name: String,
@@ -28,21 +28,21 @@ pub struct AnnotationEditor {
 
 #[allow(unused)]
 impl AnnotationEditor {
-    pub fn new(file_name : &String) -> AnnotationEditor {
-
-        let mut image=imgcodecs::imread(&file_name, 1).unwrap();
+    pub fn new(file_name: &String) -> AnnotationEditor {
+        let mut image = imgcodecs::imread(&file_name, 1).unwrap();
         // FIXME: missing image not detected
-    
+
         println!("asd {}", image.size().unwrap().width);
- 
+
         let mut points = Arc::new(Mutex::new(VectorOfPoint::new()));
 
         let mut annotation_file = file_name.clone();
         annotation_file.push_str(".annot");
-    
+
         if let Ok(f) = std::fs::File::open(&annotation_file) {
-            let data : OnDiskAnnotations = serde_yaml::from_reader(f).expect("Could not read values.");
-            for (x,y) in data.points {
+            let data: OnDiskAnnotations =
+                serde_yaml::from_reader(f).expect("Could not read values.");
+            for (x, y) in data.points {
                 points.lock().unwrap().push(Point2i::new(x, y));
             }
         } else {
@@ -56,82 +56,96 @@ impl AnnotationEditor {
             image: image,
         }
     }
-    fn save(self : &AnnotationEditor) {
-        let f=std::fs::File::create(&self.annotation_file).expect("Can't open annot file for write");
+    fn save(self: &AnnotationEditor) {
+        let f =
+            std::fs::File::create(&self.annotation_file).expect("Can't open annot file for write");
         let points = self.points.lock().unwrap();
-        let a_points = points.iter().map(|p| (p.x,p.y)).collect();
-        let ann : OnDiskAnnotations = OnDiskAnnotations { points: a_points };
+        let a_points = points.iter().map(|p| (p.x, p.y)).collect();
+        let ann: OnDiskAnnotations = OnDiskAnnotations { points: a_points };
 
         serde_yaml::to_writer(f, &ann).unwrap();
         dbg!("annot written");
     }
-    fn draw(self : &AnnotationEditor) -> Result<Mat> {
-
-        let mut frame=self.image.clone();
+    fn draw(self: &AnnotationEditor) -> Result<Mat> {
+        let mut frame = self.image.clone();
         let points2 = self.points.lock().unwrap().to_owned();
-        let mut hull_points = VectorOfPoint::new();//Mat::default();
+        let mut hull_points = VectorOfPoint::new(); //Mat::default();
         if points2.len() > 0 {
-
             convex_hull(&points2, &mut hull_points, true, true);
-            
+
             for i in 1..points2.len() {
-                let color = Scalar::new(255.,0.,255.,0.);
-                imgproc::line(&mut frame, points2.get(i-1)?, points2.get(i)?, color,2 , LINE_8, 0)?;
+                let color = Scalar::new(255., 0., 255., 0.);
+                imgproc::line(
+                    &mut frame,
+                    points2.get(i - 1)?,
+                    points2.get(i)?,
+                    color,
+                    2,
+                    LINE_8,
+                    0,
+                )?;
             }
             for i in 0..hull_points.len() {
-                let color = Scalar::new(0.,255.,0.,0.);
-                let j = if i>0 {i-1} else {hull_points.len()-1};
-                imgproc::line(&mut frame, hull_points.get(j)?, hull_points.get(i)?, color,2 , LINE_8, 0)?;
+                let color = Scalar::new(0., 255., 0., 0.);
+                let j = if i > 0 { i - 1 } else { hull_points.len() - 1 };
+                imgproc::line(
+                    &mut frame,
+                    hull_points.get(j)?,
+                    hull_points.get(i)?,
+                    color,
+                    2,
+                    LINE_8,
+                    0,
+                )?;
             }
         }
         Ok(frame)
     }
-    fn add_point(self : &AnnotationEditor, point : Point2i) {
+    fn add_point(self: &AnnotationEditor, point: Point2i) {
         self.points.lock().unwrap().push(point);
     }
 
     pub(crate) fn make_dist_map(&self, m: Mat, size: Size_<i32>, pos: Point_<i32>) -> Mat {
-            let mut dist_map = Mat::zeros_size(size, CV_64F).unwrap().to_mat().unwrap();
-            for row in 0..size.height {
-                for col in 0..size.width {
-                    let p1 =Point2i::new(col, row);
-                    let p2 =Point2i::new(col, row);
-                    let d=(p1 - pos).norm();
-                    // let p2 =Point2i::new(col+50, row+50);
-                    *dist_map.at_2d_mut::<f64>(row, col).unwrap() =
-                        (d-100.0).max(0.1)
-
-                }
+        let mut dist_map = Mat::zeros_size(size, CV_64F).unwrap().to_mat().unwrap();
+        for row in 0..size.height {
+            for col in 0..size.width {
+                let p1 = Point2i::new(col, row);
+                let p2 = Point2i::new(col, row);
+                let d = (p1 - pos).norm();
+                // let p2 =Point2i::new(col+50, row+50);
+                *dist_map.at_2d_mut::<f64>(row, col).unwrap() = (d - 100.0).max(0.1)
             }
-            dist_map
+        }
+        dist_map
     }
 }
 
-
-pub fn editor(file_name : &String) -> Result<()> { 
-
+pub fn editor(file_name: &String) -> Result<()> {
     let annotation_editor = Arc::new(Mutex::new(AnnotationEditor::new(file_name)));
     let window = "montage-gen annotate";
 
     highgui::named_window(window, highgui::WINDOW_KEEPRATIO | WINDOW_GUI_EXPANDED)?;
-    highgui::set_mouse_callback(window, Some(Box::new({
-       let annotation_editor = Arc::clone(&annotation_editor);
-        move |event, x ,y, _flags| {
-            match event {
+    highgui::set_mouse_callback(
+        window,
+        Some(Box::new({
+            let annotation_editor = Arc::clone(&annotation_editor);
+            move |event, x, y, _flags| match event {
                 EVENT_LBUTTONDOWN => {
-                    println!(" {} {} ",x,y);
-                    annotation_editor.lock().unwrap().add_point(Point2i::new(x,y));
+                    println!(" {} {} ", x, y);
+                    annotation_editor
+                        .lock()
+                        .unwrap()
+                        .add_point(Point2i::new(x, y));
                 }
-                _ => {
-                }
+                _ => {}
             }
-        }
-    })))?;
+        })),
+    )?;
 
     resize_window(window, 500, 500)?;
     loop {
         let key = highgui::wait_key(100)?;
-        if key==27 {
+        if key == 27 {
             break;
         }
         let annotation_editor = annotation_editor.lock().unwrap();
@@ -139,7 +153,7 @@ pub fn editor(file_name : &String) -> Result<()> {
             annotation_editor.save();
             break;
         }
-        let frame=annotation_editor.draw()?;
+        let frame = annotation_editor.draw()?;
         imshow(window, &frame)?;
     }
     Ok(())
