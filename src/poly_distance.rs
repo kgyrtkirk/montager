fbdf2fed6f07eaec7bfbcc5e2b2f836cf5d41c3d
Zoltan Haindrich <zhaindrich@cloudera.com>
2022-10-14 15:37:16 +0200

    polydist

--- src/poly_distance.rs
+++ src/poly_distance.rs
@@ -6,7 +6,7 @@ trait PolyDist {
     fn dist(&self, p: &Point_<f64>) -> Result<f64>;
 }
 
-// FIXME: template
+// FIXME: template?
 impl PolyDist for core::Point_<f64> {
     fn dist(&self, p: &Point_<f64>) -> Result<f64> {
         let v2 = *self - *p;
@@ -14,20 +14,36 @@ impl PolyDist for core::Point_<f64> {
     }
 }
 
+// FIXME: signed dist is only valid for convex polys
 impl PolyDist for VectorOfPoint2d {
     fn dist(&self, p: &Point_<f64>) -> Result<f64> {
         let mut dist = f64::MAX;
-        for i in 0..self.len() - 1 {
+        for i in 0..self.len() {
             let p1 = self.get(i + 0)?;
-            let p2 = self.get(i + 1)?;
+            let p2 = self.get((i + 1) % self.len())?;
 
             let mut n = p2 - p1;
-            n /= n.norm();
-
-            let r = n.cross(*p);
-
-            if dist.abs() > r.abs() {
-                dist = r;
+            let l = n.norm();
+            n /= l;
+            let v = *p - p1;
+            let vl = v.dot(n);
+            if 0.0f64 <= vl && vl <= l {
+                let r = n.cross(v);
+                if dist.abs() > r.abs() {
+                    dist = r;
+                }
+            } else {
+                if vl <= 0.0f64 {
+                    let r = p.dist(&p1)?;
+                    if dist.abs() > r.abs() {
+                        dist = r;
+                    }
+                } else {
+                    let r = p.dist(&p2)?;
+                    if dist.abs() > r.abs() {
+                        dist = r;
+                    }
+                }
             }
         }
         Ok(dist)
@@ -46,20 +62,47 @@ mod tests {
         assert_eq!(d, 2.0f64.sqrt())
     }
 
-    #[test]
-    fn test_poly_dist2() {
+    /*
+     *  2   *
+     *  |  /|
+     *  1 *-*
+     *  |
+     *  +-1-2
+     */
+    fn sample_poly() -> Vector<Point_<f64>> {
         let p1 = Point2d::new(1.0f64, 1.0f64);
         let p2 = Point2d::new(2.0f64, 2.0f64);
-        let poly = Vector::from_slice(&[p1, p2]);
-        assert_eq!(poly.dist(&p1).unwrap(), 0.0f64.sqrt());
-        assert_eq!(poly.dist(&p2).unwrap(), 0.0f64.sqrt());
+        let p3 = Point2d::new(2.0f64, 1.0f64);
+        let poly = Vector::from_slice(&[p1, p2, p3]);
+        poly
+    }
+
+    #[test]
+    fn test_poly_dist_on() {
+        let poly = sample_poly();
+
+        assert_abs_diff_eq!(poly.dist(&sample_poly().get(2).unwrap()).unwrap(), 0.0f64);
+        assert_abs_diff_eq!(poly.dist(&sample_poly().get(0).unwrap()).unwrap(), 0.0f64);
+        assert_abs_diff_eq!(poly.dist(&sample_poly().get(1).unwrap()).unwrap(), 0.0f64);
+    }
+    #[test]
+    fn test_poly_dist_left_right_in_range() {
+        let poly = sample_poly();
         assert_abs_diff_eq!(
-            poly.dist(&Point2d::new(2.0f64, 1.0f64)).unwrap(),
-            2.0f64.sqrt() / -2.0f64
+            poly.dist(&Point2d::new(1.6f64, 1.5f64)).unwrap(),
+            2.0f64.sqrt() / -20.0f64
         );
         assert_abs_diff_eq!(
             poly.dist(&Point2d::new(1.0f64, 2.0f64)).unwrap(),
             2.0f64.sqrt() / 2.0f64
+        );
+    }
+    #[test]
+    fn test_poly_dist_outside_line_end() {
+        let poly = sample_poly();
+        assert_abs_diff_eq!(
+            poly.dist(&Point2d::new(0.0f64, 0.0f64)).unwrap(),
+            2.0f64.sqrt()
         );
     }
 }
