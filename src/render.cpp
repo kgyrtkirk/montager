7a0e998ad9f49aec34c0602532a68781a61230c3
Zoltan Haindrich <zhaindrich@cloudera.com>
2022-10-24 13:16:31 +0200

    decompose single method into class

--- src/render.cpp
+++ src/render.cpp
@@ -17,72 +17,112 @@
 
 BOOST_GEOMETRY_REGISTER_BOOST_TUPLE_CS(cs::cartesian)
 
-void read_image(gint32 drawable_id)
+using namespace boost::geometry;
+using boost::geometry::model::multi_point;
+using boost::geometry::model::polygon;
+using boost::geometry::model::d2::point_xy;
+
+class PImage
 {
-	using boost::geometry::append;
-	using boost::geometry::make;
-	using boost::geometry::model::d2::point_xy;
-
-	boost::geometry::model::multi_point<point_xy<int>> points;
-
-	GimpDrawable *drawable = gimp_drawable_get(drawable_id);
-	gint bpp = drawable->bpp; //(drawable->drawable_id);
-	gint w = drawable->width;
-	gint h = drawable->height;
-
-	GimpPixelRgn region;
-	gimp_pixel_rgn_init(&region, drawable, 0, 0, w, h, FALSE, FALSE);
-	size_t size = w * h * bpp;
-	guchar *img = (guchar *)g_malloc(size);
-	// printf("maskbpp :%d %d\n", bpp, size);
-
-	gimp_pixel_rgn_get_rect(&region, img, 0, 0, w, h);
-
-	for (int x = 0; x < w; x++)
+	GimpDrawable *drawable;
+	guchar *img;
+	polygon<point_xy<int>> hull;
+	void read_image()
 	{
-		for (int y = 0; y < h; y++)
+		gint bpp = drawable->bpp;
+		gint w = drawable->width;
+		gint h = drawable->height;
+		size_t size = w * h * bpp;
+		if (bpp != 1)
 		{
-			if (img[y * w + x] >= 255)
+			printf("bpp: %d\n", bpp);
+			// g_error("bpp is expected to be 1 at this point");
+		}
+			printf("rinit %d",size);
+			printf("rinit %d",__LINE__);
+		img = (guchar *)g_malloc(size);
+			printf("rinit %d",__LINE__);
+
+		GimpPixelRgn region;
+			printf("rinit %d",__LINE__);
+		gimp_pixel_rgn_init(&region, drawable, 0, 0, w, h, FALSE, FALSE);
+			printf("rinit %d",__LINE__);
+
+		gimp_pixel_rgn_get_rect(&region, img, 0, 0, w, h);
+			printf("end-get: %d\n", bpp);
+	}
+	void compute_hull()
+	{
+		multi_point<point_xy<int>> points;
+		gint w = drawable->width;
+		gint h = drawable->height;
+
+		for (int x = 0; x < w; x++)
+		{
+			for (int y = 0; y < h; y++)
 			{
-				append(points, make<point_xy<int>>(x, y));
+				if (img[y * w + x] >= 255)
+				{
+					append(points, make<point_xy<int>>(x, y));
+				}
+			}
+		}
+
+		convex_hull(points, hull);
+		using boost::geometry::dsv;
+		std::cout << dsv(hull) << std::endl;
+	}
+
+public:
+	PImage(gint32 drawable_id)
+	{
+		drawable = gimp_drawable_get(drawable_id);
+		read_image();
+		compute_hull();
+	};
+
+	~PImage()
+	{
+		gimp_drawable_detach(drawable);
+		g_free(img);
+	}
+
+	void show_distance()
+	{
+		gint bpp = drawable->bpp;
+		gint w = drawable->width;
+		gint h = drawable->height;
+
+		size_t size = w * h * bpp;
+		memset(img, 0, size);
+		for (auto y = 0; y < h; y++)
+		{
+			for (auto x = 0; x < w; x++)
+			{
+				point_xy<int> p(x, y);
+				double d = boost::geometry::distance(p, hull);
+				guchar v;
+				if (d <= 0.0)
+					v = 255;
+				else
+					v = std::max(0.0, 254.0 - d);
+				img[p.y() * w + p.x()] = v;
 			}
 		}
 	}
-
-	boost::geometry::model::polygon<point_xy<int>> hull;
-	boost::geometry::convex_hull(points, hull);
-
-	using boost::geometry::dsv;
-	memset(img, 0, size);
-	std::cout << dsv(hull) << std::endl;
-	// for(auto it=hull.outer().begin();it!=hull.outer().end();it++){
-	// 	point_xy<int>  &p=*it;
-	// 	img[p.y()*w+p.x()]=255;
-	// }
-	for (auto y = 0; y < h; y++)
+	void flush()
 	{
-		for (auto x = 0; x < w; x++)
-		{
-			point_xy<int> p(x, y);
-			double d = boost::geometry::distance(p, hull);
-			guchar	v;
-			if(d<=0.0) 
-				v=255;
-				else
-				v=std::max(0.0,254.0-d);
-			img[p.y() * w + p.x()] = v;
-		}
+		gint w = drawable->width;
+		gint h = drawable->height;
 
+		GimpPixelRgn region;
+		gimp_pixel_rgn_init(&region, drawable, 0, 0, w, h, TRUE, TRUE);
+		gimp_pixel_rgn_set_rect(&region, img, 0, 0, w, h);
+		gimp_drawable_flush(drawable);
+		gimp_drawable_merge_shadow(drawable->drawable_id, TRUE);
+		gimp_drawable_update(drawable->drawable_id, 0, 0, w, h);
 	}
-
-	gimp_pixel_rgn_init(&region, drawable, 0, 0, w, h, TRUE, TRUE);
-	gimp_pixel_rgn_set_rect(&region, img, 0, 0, w, h);
-	gimp_drawable_flush(drawable);
-	gimp_drawable_merge_shadow(drawable->drawable_id, TRUE);
-	gimp_drawable_update(drawable->drawable_id, 0, 0, w, h);
-	gimp_drawable_detach(drawable);
-	g_free(img);
-}
+};
 
 /*  Public functions  */
 
@@ -110,7 +150,9 @@ void render(gint32 image_ID,
 		printf("name: %s  ; mask :%d  @%d,%d\n", name, mask, x, y);
 		g_message(name);
 
-		read_image(mask);
+		PImage mi(mask);
+		mi.show_distance();
+		mi.flush();
 		//		read_image(layer);
 		// gimp_drawable_get_pixel
 		// gimp_drawable_get
