ba1fe12f495f08dec5e28a6355ca32af8a6593e0
Zoltan Haindrich <kirk@rxd.hu>
2022-11-07 16:22:06 +0100

    move pimage to separate file

---
+++ src/pimage.h
@@ -0,0 +1,209 @@
+#include "config.h"
+
+#include <gtk/gtk.h>
+
+#include <libgimp/gimp.h>
+
+#include "main.h"
+#include "render.h"
+#include "image.h"
+#include "dist_queue.h"
+
+#include <iostream>
+
+#include <boost/chrono.hpp>
+#include <boost/geometry.hpp>
+#include <boost/geometry/geometries/polygon.hpp>
+#include <boost/geometry/geometries/adapted/boost_tuple.hpp>
+#include <boost/geometry/geometries/point_xy.hpp>
+#include <boost/geometry/multi/geometries/multi_point.hpp>
+
+class PImage
+{
+	using point_xy = boost::geometry::model::d2::point_xy<int>;
+	using polygon = boost::geometry::model::polygon<point_xy>;
+	GimpDrawable *drawable;
+	polygon local_hull;
+	point_xy pos;
+	polygon hull;
+	image img;
+
+private:
+	void read_image()
+	{
+		gint bpp = drawable->bpp;
+		gint w = drawable->width;
+		gint h = drawable->height;
+		size_t size = w * h * bpp;
+		if (bpp != 1)
+		{
+			g_error("bpp is expected to be 1 at this point; however its: %d", bpp);
+		}
+		img.init(w, h);
+
+		GimpPixelRgn region;
+		gimp_pixel_rgn_init(&region, drawable, 0, 0, w, h, FALSE, FALSE);
+		gimp_pixel_rgn_get_rect(&region, img.get(), 0, 0, w, h);
+	}
+	void compute_hull()
+	{
+		multi_point<point_xy> points;
+		gint w = drawable->width;
+		gint h = drawable->height;
+
+		guchar *img = this->img.get();
+		for (int y = 0; y < h; y++)
+		{
+			int g = -1;
+			for (int x = 0; x < w; x++)
+			{
+				if (img[y * w + x] >= 255)
+				{
+					if (g == -1)
+						append(points, make<point_xy>(x, y));
+					g = x;
+				}
+			}
+			if (g >= 0)
+				append(points, make<point_xy>(g, y));
+		}
+
+		convex_hull(points, local_hull);
+
+		// make global hull
+		translate_transformer<int, 2, 2> translate(pos.x(), pos.y());
+		transform(local_hull, hull, translate);
+	}
+	void extract_drawable_infos(gint32 drawable_id)
+	{
+		drawable = gimp_drawable_get(drawable_id);
+		gint x, y;
+		gimp_drawable_offsets(drawable->drawable_id, &x, &y);
+		pos = point_xy(x, y);
+	}
+
+public:
+	PImage(gint32 drawable_id)
+	{
+		extract_drawable_infos(drawable_id);
+		read_image();
+		compute_hull();
+	};
+	PImage(const PImage &other)
+	{
+		extract_drawable_infos(other.drawable->drawable_id);
+		img = other.img;
+		local_hull = other.local_hull;
+		hull = other.hull;
+	};
+
+	~PImage()
+	{
+		gimp_drawable_detach(drawable);
+	}
+	const polygon &getHull() const
+	{
+		return hull;
+	}
+
+	void paint(const point_xy &p)
+	{
+		int x = p.x() - pos.x();
+		int y = p.y() - pos.y();
+		img.paint(t_point(x, y), 254);
+	}
+
+	double distance(const point_xy &p) const
+	{
+		return boost::geometry::distance(p, hull);
+	}
+	void show_distance()
+	{
+		gint bpp = drawable->bpp;
+		gint w = drawable->width;
+		gint h = drawable->height;
+		guchar *img = this->img.get();
+
+		size_t size = w * h * bpp;
+		memset(img, 0, size);
+		for (auto y = 0; y < h; y++)
+		{
+			for (auto x = 0; x < w; x++)
+			{
+				point_xy p(x, y);
+				double d = boost::geometry::distance(p, local_hull);
+				guchar v;
+				if (d <= 0.0)
+					v = 255;
+				else
+					v = std::max(0.0, 254.0 - d);
+				img[p.y() * w + p.x()] = v;
+			}
+		}
+	}
+	void debug()
+	{
+		using boost::geometry::dsv;
+		std::cout << dsv(hull) << std::endl;
+	}
+	void cleanup()
+	{
+		gint w = drawable->width;
+		gint h = drawable->height;
+		guchar *img = this->img.get();
+
+		for (auto y = 0; y < h; y++)
+		{
+			for (auto x = 0; x < w; x++)
+			{
+				int v = img[y * w + x];
+				if (v < 255)
+					v = 0;
+				img[y * w + x] = v;
+			}
+		}
+	}
+	void show_hull()
+	{
+		gint w = drawable->width;
+		gint h = drawable->height;
+		guchar *img = this->img.get();
+
+		for (auto y = 0; y < h; y++)
+		{
+			for (auto x = 0; x < w; x++)
+			{
+				int v = img[y * w + x];
+				if (v >= 255)
+					continue;
+
+				point_xy p(x, y);
+				if (within(p, local_hull))
+				{
+					v = 254;
+				}
+				else
+				{
+					v = 0;
+				}
+				img[y * w + x] = v;
+			}
+		}
+	}
+	void flush()
+	{
+		gint w = drawable->width;
+		gint h = drawable->height;
+
+		GimpPixelRgn region;
+		gimp_pixel_rgn_init(&region, drawable, 0, 0, w, h, TRUE, TRUE);
+		gimp_pixel_rgn_set_rect(&region, img.get(), 0, 0, w, h);
+		gimp_drawable_flush(drawable);
+		gimp_drawable_merge_shadow(drawable->drawable_id, TRUE);
+		gimp_drawable_update(drawable->drawable_id, 0, 0, w, h);
+	}
+	GimpDrawable *getDrawable()
+	{
+		return drawable;
+	}
+};
