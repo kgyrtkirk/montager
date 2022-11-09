fa109b817084195cfac868bf24d4d1d9845e0991
Zoltan Haindrich <kirk@rxd.hu>
2022-11-09 12:34:17 +0100

    try out different eq for forces

--- src/fd_layout.cpp
+++ src/fd_layout.cpp
@@ -126,16 +126,19 @@ t_point fd_layout::compute_force(entry *l, entry *r)
     {
         d = .00000000001 * (random() % 1000);
     }
-    if(true) {
+    if(false) {
 
-        double mag=10*log(d/50.0)/max_step;
-        if (mag > 1.0)
-            mag = 1.0;
-        if (mag < -1.0)
-            mag = -1.0;
+        double mag=10*log((d+1)/30.0)/max_step;
+        // if (mag > 1.0)
+        //     mag = 1.0;
+        // if (mag < -1.0)
+        //     mag = -1.0;
         return dir * mag;
     }else {
-        return dir * (-10000 / (10 + d) / max_step);
+        // double mag=(-10000 / (30 + d*d) / max_step);
+        int O=50;
+        double mag=(-O / (O + d*d) );
+        return dir * mag;
     }
 }
 
